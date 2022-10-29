f2d543f442e36158974a8d3c50594955882bc098
SamCrav <104194706+SamCrav@users.noreply.github.com>
2022-10-29 09:58:19 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,152 @@
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente non si è ancora registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la voce “Iscriviti"
+2. Il sistema mostra una form dove inserire i dati relativi all'iscrizione (username, password)
+3. Il sistema inserirsce i dati e avvia la registrazione
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● La registrazione è avvenuta con successo.
+*/
+function iscriviti()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente sono è già registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la voce “Login Utente”
+2. Il sistema mostra una form dove inserire i dati relativi al login (username, password)
+3. Il sistema controlla le credenziali nel database
+4. Il sistema invia una risposta affermativa o negativa
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● Il login è avvenuto con successo
+*/
+function accesso()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente si è iscritto o si è registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la barra di ricerca
+2. Il sistema permette di far scrivere all'utente l'ingrediente
+3. Il sistema inserirsce i dati e avvia la ricerca
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● La ricerca è avvenuta con successo.
+*/
+function barraRicerca()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato.
+● L'utente si è iscritto o si è registrato.
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona le proprie ricette.
+2. Il sistema permette di visualizzare le proprie ricette.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento.
+Postcondizioni:
+● Visualizzazione ricette.
+*/
+function leMieRicette()
+{
+
+}
+
+/**/
+function ricettaAmici()
+{
+
+}
+
+/**/
+function creaRicetta()
+{
+
+}
+
+/**/
+function aggiungiRicetta()
+{
+
+}
+
+/**/
+function pianificazionePasto()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente si è iscritto o si è registrato
+● L'utente ha messo ricette nei preferiti
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la voce preferiti.
+2. Il sistema permette di visualizzare le ricette inserite nei preferiti.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● Visualizzazione ricette nel campo preferiti.
+*/
+function preferiti()
+{
+
+}
+
+/**/
+function categorieGruppi()
+{
+
+}
+
+/**/
+function impostazioni()
+{
+
+}
+
+/**/
+function guida()
+{
+
+}
+
+/**/
+function info()
+{
+    
+}
