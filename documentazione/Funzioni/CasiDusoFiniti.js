c9bae68d68c895c71eed8de74ed1262feff7c408
m1ncio <lorenzominciotti11@gmail.com>
2022-11-08 09:31:02 +0100

    Aggiornamento struttura directory github

---
+++ documentazione/Funzioni/CasiDusoFiniti.js
@@ -0,0 +1,207 @@
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente non si è ancora registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la voce “Iscriviti"
+2. Il sistema mostra una form dove inserire i dati relativi all'iscrizione (username, password)
+3. Il sistema inserirsce i dati e avvia la registrazione
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● La registrazione è avvenuta con successo.
+*/
+function iscriviti()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente sono è già registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la voce “Login Utente”
+2. Il sistema mostra una form dove inserire i dati relativi al login (username, password)
+3. Il sistema controlla le credenziali nel database
+4. Il sistema invia una risposta affermativa o negativa
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● Il login è avvenuto con successo
+*/
+function accesso()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente si è iscritto o si è registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la barra di ricerca
+2. Il sistema permette di far scrivere all'utente l'ingrediente
+3. Il sistema inserirsce i dati e avvia la ricerca
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● La ricerca è avvenuta con successo.
+*/
+function barraRicerca()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato.
+● L'utente si è iscritto o si è registrato.
+● Hai in mente delle ricette da creare.
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona crea ricette.
+2. Il sistema permette di creare le proprie ricette.
+3. Il sistema permette di far vedere agli altri le proprie ricette.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento.
+Postcondizioni:
+● Creazione delle proprie ricette.
+*/
+function creaRicetta()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato.
+● L'utente si è iscritto o si è registrato.
+● Hai creato delle ricette.
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona le proprie ricette.
+2. Il sistema permette di visualizzare le proprie ricette.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento.
+Postcondizioni:
+● Visualizzazione ricette.
+*/
+function leMieRicette()
+{
+
+}
+
+/*ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato.
+● L'utente si è iscritto o si è registrato.
+● Hai degli amici nell'app.
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona le ricette degli amici.
+2. Il sistema permette di visualizzare le ricette dei propri amici.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento.
+Postcondizioni:
+● Visualizzazione ricette amici.
+*/
+function ricettaAmici()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato.
+● L'utente si è iscritto o si è registrato.
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona per vedere la pianificazione.
+2. Il sistema permette di visualizzare la propria pianificazione dei pasti.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento.
+Postcondizioni:
+● Visualizzazione calendario/pianificazione.
+*/
+function pianificazionePasto()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente si è iscritto o si è registrato
+● L'utente ha messo ricette nei preferiti
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la voce preferiti.
+2. Il sistema permette di visualizzare le ricette inserite nei preferiti.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● Visualizzazione ricette nel campo preferiti.
+*/
+function preferiti()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente si è iscritto o si è registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona le impostazioni.
+2. Il sistema permette di visualizzare le impostazioni dell'app.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● Visualizzazione delle impostazioni dell'app.
+*/
+function impostazioni()
+{
+
+}
+
+/*
+ID: UC_LoginUtente
+Attori:
+Utente
+Precondizioni:
+● Il sistema è stato avviato
+● L'utente si è iscritto o si è registrato
+Sequenza eventi:
+1. Il caso d'uso inizia quando l'utente seleziona la guida.
+2. Il sistema permette di visualizzare la guida dell'app.
+Sequenze alternative:
+1. L'utente può abbandonare la funzionalità in qualsiasi momento
+Postcondizioni:
+● Visualizzazione della guida dell'app.
+*/
+function guida()
+{
+
+}
